bd0293332ace602554023b1b37a1e321ea164503
Ubuntu <azureuser@openclaw-vm.uze3mnxy05jezkcrix5ryklr2h.bx.internal.cloudapp.net>
2026-03-23 10:24:13 +0000

    Bump version to 18 to force cache update

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ let authMode = 'login';
 let currentAudioFallbackUrls = [];
 let currentAudioFallbackIndex = 0;
 let currentSongStreamRefreshed = false;
-const RELEASE_MARKER = '17';
+const RELEASE_MARKER = '18';
 const AAC_CODEC = 'audio/mp4; codecs="mp4a.40.2"';
 let hasShownCodecWarning = false;
 
